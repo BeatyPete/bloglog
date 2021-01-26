6cdd8706a52c224e4c7f4555032afa31a48dadd4
BeatyPete <akira941@gmail.com>
2021-01-25 19:36:39 -0600

    user table created with get and post api endpoints

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS bloglogna_db;
+
+CREATE DATABASE bloglogna_db;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+Use bloglogna_db
+
+INSERT INTO users (username, email, password)
+VALUES
+    ('beatypete', 'akira941@gmail.com', 'doraemon');
