6dd8e8b3cd304076414915c6c7edc09288b289e4
BeatyPete <akira941@gmail.com>
2021-01-28 00:08:38 -0600

    can now add posts from the dashboard

---
+++ public/javascript/new-post.js
@@ -0,0 +1,68 @@
+const postContainerEl = document.querySelector(".post-container");
+
+
+const populateEditor = function() {
+    postContainerEl.innerHTML = "";
+
+    const editorContainerEl =document.createElement("article");
+    const editorHeaderEl =document.createElement("div");
+    editorHeaderEl.classList = "post-header";
+    editorHeaderEl.innerHTML = '<h1 class="post-title">Create New Post</h1>';
+    editorContainerEl.appendChild(editorHeaderEl)
+
+    const editorFormEl = document.createElement('form');
+    editorFormEl.classList = 'login-form'
+    const titleInputEl = document.createElement('div')
+    titleInputEl.innerHTML = '<label for="title">Title</label><input type="text" id="title" name="title" />'
+    editorFormEl.appendChild(titleInputEl)
+
+    const contentInputEl = document.createElement('div')
+    contentInputEl.innerHTML = '<label for="content">Content</label><input type="text" id="content" name="content" />'
+    editorFormEl.appendChild(contentInputEl)
+
+    const postBtnnEl = document.createElement('div');
+    postBtnnEl.innerHTML = '<button class="login-btn" type="submit">Create</button>'
+    editorFormEl.appendChild(postBtnnEl)
+
+    editorContainerEl.appendChild(editorFormEl)
+
+    postContainerEl.appendChild(editorContainerEl);
+}
+
+async function newFormHandler(event) {
+    event.preventDefault();
+    const elem = event.target
+
+    if (elem.classList.contains('login-form')) {
+        const title = document.querySelector('input[name="title"]').value;
+        const post_text = document.querySelector('input[name="content"]').value;
+    
+        const response = await fetch(`/api/posts`, {
+            method: 'POST',
+            body: JSON.stringify({
+                title,
+                post_text
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+postContainerEl.addEventListener( "submit", newFormHandler );
+
+  function someListener(event){
+      var element = event.target;
+      if(element.tagName == 'A' && element.classList.contains("someBtn")){
+          console.log("hi");
+      }
+  }
+
+document.querySelector('.post-btn').addEventListener('click', populateEditor);
